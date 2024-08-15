22d149fc7105bd80df27f47d7d1bf9ea2253568b
Aaron Watters <awatters@flatironinstitute.org>
2024-08-15 15:01:40 -0400

    more testing infrastructure

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,52 @@
-// based on https://andrewwalpole.com/blog/use-vite-for-javascript-libraries/
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { configDefaults } from 'vitest/config';
 
-const path = require('path')
-const { defineConfig } = require('vite')
-
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    exclude: [
+      ...configDefaults.exclude, 
+      'tests/e2e/*', // Exclude e2e tests if you have any
+    ],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'tests/**/*',
+        'dist/**/*',
+        "*.js", // Exclude all JavaScript files in the root directory
+      ],
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'),
+      name: 'webgpu_volume',
+      fileName: (format) => `webgpu_volume.${format}.js`
+    },
+  },
+  // Other Vite configuration options...
+  server: {
+    port: 5555, // Configures the development server to use port 3000
+  },
+  preview: {
+    port: 5555, // Configures the preview server to use port 3000
+  },
+});
+/*
 module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'lib/main.js'),
       name: 'webgpu_volume',
       fileName: (format) => `webgpu_volume.${format}.js`
-    },
-    minify: false
+    }
   }
 });
+*/
